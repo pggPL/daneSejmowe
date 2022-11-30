9c7a00fcec65b28cb3d44f9cd62907d07baef6ff
Paweł Gadziński <pawelgadzinski@gmail.com>
2022-11-30 17:38:36 +0000

    init

---
+++ ddl.sql
@@ -0,0 +1,66 @@
+-- Drop tables if they do not exist.
+
+DROP TABLE IF EXISTS "member_of_parliament";
+DROP TABLE IF EXISTS "club";
+DROP TABLE IF EXISTS "speech";
+DROP TABLE IF EXISTS "session";
+DROP TABLE IF EXISTS "voting";
+DROP TABLE IF EXISTS "vote";
+
+
+CREATE TABLE "member_of_parliament" (
+    "id" int4 NOT NULL PRIMARY KEY,
+    "name" varchar(255) NOT NULL,
+    "day_of_election" date NOT NULL,
+    "list" varchar(255) NOT NULL,
+    "district" varchar(255) NOT NULL,
+    "number_of_votes" int4 NOT NULL,
+    "date_of_oath" date NOT NULL,
+    "experience_in_parliament" varchar(255) NOT NULL,
+    "function_in_club" varchar(255) NOT NULL,
+    "date_of_birth" int4 NOT NULL,
+    "place_of_birth" varchar(255) NOT NULL,
+    "education" varchar(255) NOT NULL,
+    "finished_school" varchar(255) NOT NULL,
+    "profession" varchar(255) NOT NULL
+);
+
+CREATE TABLE "club" (
+    "id" int4 NOT NULL PRIMARY KEY,
+    "name" varchar(255) NOT NULL
+);
+
+ALTER TABLE "member_of_parliament" 
+ADD COLUMN "club_id" int4 NOT NULL REFERENCES "club" ("id");
+
+CREATE TABLE "speech" (
+    "id" int4 NOT NULL PRIMARY KEY,
+    "text" text NOT NULL,
+    "member_of_parliament_id" int4 NOT NULL REFERENCES "member_of_parliament" ("id")
+);
+
+CREATE TABLE "session" (
+    "number" int4 NOT NULL PRIMARY KEY,
+    "when" varchar(255) NOT NULL
+);
+
+ALTER TABLE "speech"
+ADD COLUMN "session_number" int4 NOT NULL REFERENCES "session" ("number");
+
+CREATE TABLE "voting" (
+    "id" int4 NOT NULL PRIMARY KEY,
+    "name" varchar(255) NOT NULL
+);
+
+ALTER TABLE "voting"
+ADD COLUMN "session_number" int4 NOT NULL REFERENCES "session" ("number");
+
+CREATE TABLE "vote" (
+    "id" int4 NOT NULL PRIMARY KEY,
+    "type" varchar(255) NOT NULL,
+    "member_of_parliament_id" int4 NOT NULL REFERENCES "member_of_parliament" ("id"),
+    "voting_id" int4 NOT NULL REFERENCES "voting" ("id"),
+    "club_of_the_mp_at_the_time" int4 NOT NULL REFERENCES "club" ("id"),
+
+    CONSTRAINT vote_type CHECK (type IN ('za', 'przeciw', 'nieobecny', 'wstrzymano się')) 
+);
