1d68adb3df6a62aa0df01c25dc8bd600b7304d48
pawelgadzinski <pawelgadzinski@gmail.com>
2022-12-02 00:47:32 +0100

    fr

--- public_html/district_list.php
+++ public_html/district_list.php
@@ -53,7 +53,7 @@
         $result = pg_exec($conn, $sql);
 
         while ($row = pg_fetch_row($result)) {
-            echo "<a href='district.php?number=".substr($row[0], 0, 2)."'>$row[0]</a><br>";
+            echo "<a href='district.php?number=".substr($row[0], 0, 2)."'>$row[0]</a><br><br>";
         }
 
 
